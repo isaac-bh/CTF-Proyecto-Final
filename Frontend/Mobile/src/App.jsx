15888246376e5c54ca1312c28c4f293d762f9e7a
Jonhyble <jonhy3-valadez@hotmail.com>
2023-05-21 13:36:03 -0600

    cambio de ip

--- Frontend/Mobile/src/App.jsx
+++ Frontend/Mobile/src/App.jsx
@@ -35,7 +35,7 @@ export const App = () => {
       formData.append("image", img.file);
       await axios({
         method: "POST",
-        url: "http://localhost:8000/decode",
+        url: "http://ctf-backend:8000/decode",
         data: formData,
       }).then((response) => {
         document.getElementById("dropzone-file2").value = response.data.message;
@@ -60,7 +60,7 @@ export const App = () => {
             formData.append("msg", texto)
             await axios({
               method: "POST",
-              url: "http://localhost:8000/encode",
+              url: "http://ctf-backend:8000/encode",
               data: formData,
               responseType: 'arraybuffer'
             }).then((response) => {
